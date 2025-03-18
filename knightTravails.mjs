c782ba32b38c3823705f1729ea105bbc2f491e0d
stephenwiafe <swiafe.dev@gmail.com>
2025-03-18 06:06:54 -0400

    Implement findPath

--- knightTravails.mjs
+++ knightTravails.mjs
@@ -1,8 +1,43 @@
 import { createBoard } from "./board.mjs";
 
 function knightMoves(start, end){
-   const knightBoard = createBoard();
-   return;
+   const board = createBoard();
+   let prev = findPath(start,board);
+   return reconstructPath(start,end,prev);
+}
+
+
+function findPath(start,board){
+   let queue = [];
+   let visited =[];
+   let prev = [];
+
+   for(let i = 0; i <= 7; i++){
+     visited[i].push(Array.from({length: 8}).fill(false));
+   }
+  
+   queue.push(start);
+   visited[start[0]][start[1]] = true;
+ 
+   for(let i = 0; i <= 7; i++){
+      prev[i].push(Array.from({length: 8}).fill(null));
+   }
+
+   while(queue.length > 0){
+      let currNode = queue.shift();
+      let neighbors = JSON.parse(board.get(JSON.stringify(start)));
+
+      for(let i = 0; i < neighbors.length; i++){
+         let index = neighbors[i];
+
+         if(!visited[index[0]][index[1]]){
+            queue.push([index[0],index[1]]);
+            visited[index[0]][index[1]] = true;
+            prev[index[0]][index[1]] = currNode;
+         }
+      }
+   }
+   return prev;
 }
 
 export {knightMoves};
